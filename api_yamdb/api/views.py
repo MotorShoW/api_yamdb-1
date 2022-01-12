734afc266c9a566cfd11447a9277ebccea97151a
Сергей <kuhtasergei@mail.ru>
2022-01-12 05:24:40 +0300

    First Release

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 from models.models import Titles, Genre, Category
-from .serializers import (TitlesSerializer, GenreSerializer,
-                          CategorySerializer)
+from .serializers import (TitleCreateSerializer, TitlesSerializer,
+                          GenreSerializer, CategorySerializer)
 from .permissions import IsAdmin, ReadOnly
 
 
@@ -12,6 +15,17 @@ class GenreViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
 
+    @action(
+        detail=False, methods=['delete'],
+        url_path=r'(?P<slug>\w+)',
+        lookup_field='slug', url_name='category_slug'
+    )
+    def get_genre(self, request, slug):
+        category = self.get_object()
+        serializer = CategorySerializer(category)
+        category.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -20,6 +34,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     serach_fields = ('name',)
 
+    @action(
+        detail=False, methods=['delete'],
+        url_path=r'(?P<slug>\w+)',
+        lookup_field='slug', url_name='category_slug'
+    )
+    def get_category(self, request, slug):
+        category = self.get_object()
+        serializer = CategorySerializer(category)
+        category.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
 
 class TitlesViewSet(viewsets.ModelViewSet):
     queryset = Titles.objects.all()
@@ -27,3 +52,8 @@ class TitlesViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdmin, ReadOnly]
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH'):
+            return TitleCreateSerializer
+        return TitlesSerializer
