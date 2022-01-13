abf8fb9468d84e1fbd176842ccdd31af22076bf3
Сергей <kuhtasergei@mail.ru>
2022-01-14 01:00:07 +0300

    line_b done

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -4,26 +4,15 @@ from api.models.models import Titles
 
 
 class TitleFilter(filters.FilterSet):
-    category = filters.CharFilter(
-        field_name='category__slug',
-    )
-    genre = filters.CharFilter(
-        field_name='genre__slug',
-    )
     name = filters.CharFilter(
         field_name='name',
         lookup_expr='icontains',
     )
-    year = filters.NumberFilter(
-        field_name='year',
-    )
+    year = filters.NumberFilter(field_name='year',)
+    category = filters.CharFilter(field_name='category__slug',)
+    genre = filters.CharFilter(field_name='genre__slug',)
 
     class Meta:
         model = Titles
 
-        fields = (
-            'category',
-            'genre',
-            'year',
-            'name',
-        )
+        fields = ('name', 'year', 'category', 'genre')
