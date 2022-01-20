eee5edbf10df01d681028b3bad28634313f7c8c5
Nizzerato <degterev57@gmail.com>
2022-01-20 22:47:09 +0300

    admin now in reviews app

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Category, Comment, Genre, Review, Title, User
 
 
 @admin.register(User)
@@ -13,5 +13,59 @@ class UserAdmin(admin.ModelAdmin):
         'bio',
         'email',
         'role',
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'text',
+        'author',
+        'score',
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'review',
+        'text',
+        'author',
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'genre',
+        'category',
+        'year',
+        'description',
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
     )
     empty_value_display = '-empty-'
